c88fdd7ecb9bdbac16948026d035709ba3b63b2f
gitesh <gitesh.purbiya22@gmail.com>
2014-06-29 17:03:47 +0530

    initial commit

---
+++ bonfire/themes/default/parts/_header.php
@@ -0,0 +1,16 @@
+<?php
+	// Setup our default assets to load.
+	Assets::add_js(array( 'bootstrap.min.js','lib/handsontable/jquery.handsontable.full.js','jquery.dataTables.js' ));
+	Assets::add_css( array('bootstrap.min.css', 'bootstrap-responsive.min.css','js/lib/handsontable/jquery.handsontable.full.css','bootstrap-dataTables.css'));
+			
+	$inline  = '$(".dropdown-toggle").dropdown();';
+	$inline .= '$(".tooltips").tooltip();';
+	$inline .= '$(".login-btn").click(function(e){ e.preventDefault(); $("#modal-login").modal(); });';
+	$inline .= '$("#data-table").dataTable();';
+
+	Assets::add_js( $inline, 'inline' );
+
+	Template::block('header', 'parts/head');
+
+	Template::block('topbar', 'parts/topbar');
+?>
